78edfa837978626d566f0e109f0ed7cdff38e54c
Rupesh <roopaceghimire@gmail.com>
2022-06-23 11:38:55 +0545

    Author and news source added

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 
 export default class News extends React.Component {
+	static defaultProps = {
+		country : 'in',
+		category: 'entertainment',
+		pageSize : 5
+	}
+
+	static propTypes = {
+		country : PropTypes.string,
+		catrgory : PropTypes.string,
+		pageSize : PropTypes.number
+	}
+
 	constructor() {
 		super();
 		this.state = {
@@ -13,15 +26,15 @@ export default class News extends React.Component {
 	}
 
 	async componentDidMount() {  // this function won't be invoked until promise is resolved
-		let url = `https://newsapi.org/v2/top-headlines?category=sports&country=in&q=cricket&pageSize=${this.props.pageSize}&apiKey=0f27f7d55fbd48828bd81517cd58917d`;
+		let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&pageSize=${this.props.pageSize}&category=${this.props.category}&apiKey=0f27f7d55fbd48828bd81517cd58917d`;
 		this.setState({ loading: true });
-		let data = await fetch(url);  // fetchapi returns a promise
+		let data = await fetch(url);  // fetchapi starts a request and returns a promise
 		let parsedData = await data.json();
 		this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
 	}
 
 	handlePreviousClick = async () => {
-		let url = `https://newsapi.org/v2/top-headlines?category=sports&country=in&q=cricket&apiKey=0f27f7d55fbd48828bd81517cd58917d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+		let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0f27f7d55fbd48828bd81517cd58917d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
 		this.setState({ loading: true });
 		let data = await fetch(url);
 		let parsedData = await data.json();
@@ -33,12 +46,11 @@ export default class News extends React.Component {
 	}
 
 	handleNextClick = async () => {
-		if (!this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
+		if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
 		}
 
 		else {
-			let url = `https://newsapi.org/v2/top-headlines?category=sports&country=in&q=cricket&apiKey=0f27f7d55fbd48828bd81517cd58917d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-			console.log("next button clicked");
+			let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0f27f7d55fbd48828bd81517cd58917d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
 			this.setState({ loading: true });
 			let data = await fetch(url);
 			let parsedData = await data.json()
@@ -54,12 +66,12 @@ export default class News extends React.Component {
 		return (
 			<>
 				<div className="container mt-3">
-					<h1 className='text-center'>CricNews Top Headlines</h1>
+					<h1 className='text-center'>NayaKhabar Top Headlines</h1>
 					{this.state.loading && <Spinner />}
 					<div className="row my-4">
-						{this.state.articles.map((element) => {
+						{!this.state.loading&&this.state.articles.map((element) => {
 							return <div className="col-md-4 my-3" key={element.url}>
-								<NewsItem title={element.title ? element.title.slice(0, 45) : ""} desc={element.description ? element.description.slice(0, 88) : ""} image={element.urlToImage ? element.urlToImage : "https://th.bing.com/th/id/OIP.DgXrjoUlbkhALiHYlDf35gHaEw?pid=ImgDet&rs=1"} newsUrl={element.url} />
+								<NewsItem title={element.title ? element.title.slice(0, 45) : ""} desc={element.description ? element.description.slice(0, 88) : ""} image={element.urlToImage ? element.urlToImage : "https://th.bing.com/th/id/OIP.DgXrjoUlbkhALiHYlDf35gHaEw?pid=ImgDet&rs=1"} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
 							</div>
 
 						})}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 export default class NewsItem extends React.Component {
 	render() {
-		let {title,desc,image,newsUrl} = this.props;
+		let {title,desc,image,newsUrl,author,date,source} = this.props;
 		return (
 			<div>
 				<div className="card" style={{width: '18rem'}}>
 					<img src={image} className="card-img-top" alt="..."/>
 					<div className="card-body">
+						<span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source} </span>
 						<h5 className="card-title">{title} ...</h5>
 						<p className="card-text">{desc} ....</p>
+						<p className="card-text"><small className="text-muted">By {author ? author : 'Unknown'} on {new Date(date).toGMTString()}</small></p>
 						<a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
 					</div>
 				</div>
